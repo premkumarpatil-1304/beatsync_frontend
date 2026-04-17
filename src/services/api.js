bb949a06459c8fe75309c613ed1a4d7357bb6af8
prem <13premkp@gmail.com>
2026-04-17 15:29:07 +0530

    link

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://beatsync-backend-6.onrender.com';
 
 export const createRoom = async (username) => {
     const response = await axios.post(`${API_BASE_URL}/room/create`, {
