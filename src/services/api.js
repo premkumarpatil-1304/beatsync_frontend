8a259db2d4900252d3bcb2e1c43a6a759c0b996c
prem <13premkp@gmail.com>
2026-04-17 15:55:40 +0530

    final link 2

--- src/services/api.js
+++ src/services/api.js
@@ -40,5 +40,5 @@ export const uploadUrl = async (roomId, url) => {
     return response.data;
 };
 
-export const WS_BASE_URL = import.meta.env.VITE_WS_BASE_URL;
+export const WS_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export { API_BASE_URL };
